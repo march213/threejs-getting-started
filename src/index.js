0ed5c5f186fe37c3b8d2edb07a6599c449e20eab
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-01-21 23:16:00 -0500

    simple object transformations

--- src/index.js
+++ src/index.js
@@ -4,23 +4,37 @@ import './styles.css'
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.set(0, 1, 0)
+group.scale.y = 2
+group.rotation.y = Math.PI * 0.1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+cube2.position.x = -1.5
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+cube3.position.x = 1.5
+group.add(cube3)
 
 // Camera
 const sizes = { width: 800, height: 600 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 2
+// have to specify the camera position, by default all objects start at the center
+camera.position.z = 3
 scene.add(camera)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
 
 // Renderer
 
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-})
+const renderer = new THREE.WebGLRenderer({ canvas })
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
