bb424cd1d3a5e2c475ba0d65296742f764e75b66
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-01-23 21:36:16 -0500

    camera explorations

---
+++ src/index-animation.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+import './styles.css'
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const group = new THREE.Group()
+group.position.set(0, 0, 0)
+group.scale.y = 2
+group.rotation.y = Math.PI * 0.1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+cube2.position.x = -1.5
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+cube3.position.x = 1.5
+group.add(cube3)
+
+// Camera
+const sizes = { width: 800, height: 600 }
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// have to specify the camera position, by default all objects start at the center
+camera.position.z = 3
+scene.add(camera)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+
+const tl = gsap.timeline({ repeat: -1 })
+tl.to(group.position, { duration: 2, x: 2 })
+tl.to(group.position, { duration: 2, x: 0 })
+
+// Animations
+const tick = () => {
+  // Update objects
+
+  // Render
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+tick()
--- src/index.js
+++ src/index.js
@@ -1,52 +1,49 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
 import './styles.css'
 
 const canvas = document.querySelector('.webgl')
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// Scene
 const scene = new THREE.Scene()
 
-const group = new THREE.Group()
-group.position.set(0, 1, 0)
-group.scale.y = 2
-group.rotation.y = Math.PI * 0.1
-scene.add(group)
-
-const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
-cube2.position.x = -1.5
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
-cube3.position.x = 1.5
-group.add(cube3)
+// Object
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+scene.add(mesh)
 
 // Camera
-const sizes = { width: 800, height: 600 }
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// have to specify the camera position, by default all objects start at the center
-camera.position.z = 3
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const aspectRation = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(-1 * aspectRation, 1 * aspectRation, 1, -1, 1, 100)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
-// Axes helper
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
-
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas })
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
 renderer.setSize(sizes.width, sizes.height)
 
-const tl = gsap.timeline({ repeat: -1 })
-tl.to(group.position, { duration: 2, x: 2 })
-tl.to(group.position, { duration: 2, x: 0 })
+// Animate
+const clock = new THREE.Clock()
 
-// Animations
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
   // Update objects
+  mesh.rotation.y = elapsedTime
 
   // Render
   renderer.render(scene, camera)
+
+  // Call tick again on the next frame
   window.requestAnimationFrame(tick)
 }
 
