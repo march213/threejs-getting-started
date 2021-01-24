85d24d2ba7a4fc85984838be2dce78cf367b91d9
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-01-24 17:50:50 +0000

    use orbit controls to control camera

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './styles.css'
 
 // Cursor
@@ -13,6 +14,7 @@ window.addEventListener('mousemove', (e) => {
 })
 
 const canvas = document.querySelector('.webgl')
+
 // Sizes
 const sizes = {
   width: 800,
@@ -36,6 +38,11 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -51,12 +58,8 @@ const tick = () => {
   // Update objects
   // mesh.rotation.y = elapsedTime
 
-  // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  camera.position.y = cursor.y * 5
-
-  camera.lookAt(mesh.position)
+  // Update controls
+  controls.update()
 
   // Render
   renderer.render(scene, camera)
