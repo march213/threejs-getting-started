0a2ba9669c33aa4aab3bf82eadc766f87ee93795
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-05-08 19:09:06 -0400

    use alpa texture for the material to imitate shadow; apply animation to a sphere

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css'
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const bakedShadowTexture = textureLoader.load(require('./assets/textures/bakedShadow.jpg'))
+const shadowTexture = textureLoader.load(require('./assets/textures/simpleShadow.jpg'))
 
 /**
  * Base
@@ -102,12 +102,25 @@ gui.add(material, 'roughness').min(0).max(1).step(0.001)
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
 sphere.castShadow = true
 
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), new THREE.MeshBasicMaterial({ map: bakedShadowTexture }))
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
 plane.rotation.x = -Math.PI * 0.5
 plane.position.y = -0.5
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1.5, 1.5),
+  new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    alphaMap: shadowTexture,
+  })
+)
+sphereShadow.rotation.x = -Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphereShadow)
 
 /**
  * Sizes
@@ -164,6 +177,11 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update the sphere
+  sphere.position.x = Math.cos(elapsedTime) * 1.5
+  sphere.position.z = Math.sin(elapsedTime) * 1.5
+  sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
 
   // Update controls
   controls.update()
