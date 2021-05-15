6f64a86866ac7618cc7ee86c0a0c54e5e063e81d
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-05-15 09:53:54 -0400

    added door, booshes

--- src/index.js
+++ src/index.js
@@ -34,10 +34,58 @@ walls.position.y = houseHeight / 2
 house.add(walls)
 
 // Roof
-const roof = new THREE.Mesh(new THREE.ConeBufferGeometry(3.5, 1, 4), new THREE.MeshStandardMaterial({ color: 0xb35f45 }))
-roof.position.y = houseHeight + 1 / 2
+const roofHeight = 1
+const roof = new THREE.Mesh(new THREE.ConeBufferGeometry(3.5, roofHeight, 4), new THREE.MeshStandardMaterial({ color: 0xb35f45 }))
+roof.position.y = houseHeight + roofHeight / 2
 roof.rotation.y = Math.PI * 0.25
 house.add(roof)
+
+// Door
+const doorHeight = 2
+const door = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, doorHeight), new THREE.MeshStandardMaterial({ color: 0xaa7b7b }))
+door.position.y = doorHeight / 2
+door.position.z = 2.001
+house.add(door)
+
+// Bushes
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: 0x89c854 })
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
+house.add(bush1, bush2, bush3, bush4)
+
+// Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: 0xb2b6b1 })
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2
+  const x = Math.sin(angle)
+  const z = Math.cos(angle)
+
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+  grave.position.x = x
+  grave.position.z = z
+  graves.add(grave)
+}
 
 // Floor
 const floor = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), new THREE.MeshStandardMaterial({ color: '#a9c388' }))
