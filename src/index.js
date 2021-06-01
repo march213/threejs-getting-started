d4b16ee624542b0dc6eee488218f0af0cc962367
Eugenia Molodetskaya <jane.molodetskaya@gmail.com>
2021-06-01 09:52:58 -0400

    reset project

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css'
  * Base
  */
 // Debug
-const gui = new dat.GUI({ width: 360 })
+const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -16,92 +16,17 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Galaxy
+ * Objects
  */
-const parameters = {}
-parameters.count = 100000
-parameters.size = 0.001
-parameters.radius = 5
-parameters.branches = 3
-parameters.spin = 1
-parameters.randomness = 0.2
-parameters.randomnessPower = 3
-parameters.insideColor = '#ff6030'
-parameters.outsideColor = '#1b3984'
+const object1 = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), new THREE.MeshBasicMaterial({ color: '#ff0000' }))
+object1.position.x = -2
 
-let geometry = null
-let material = null
-let points = null
+const object2 = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), new THREE.MeshBasicMaterial({ color: '#ff0000' }))
 
-const generateGalaxy = () => {
-  // Destroy old galaxy
-  if (points !== null) {
-    geometry.dispose()
-    material.dispose()
-    scene.remove(points)
-  }
+const object3 = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), new THREE.MeshBasicMaterial({ color: '#ff0000' }))
+object3.position.x = 2
 
-  // Geometry
-  geometry = new THREE.BufferGeometry()
-  const positions = new Float32Array(parameters.count * 3)
-  const colors = new Float32Array(parameters.count * 3)
-
-  const colorInside = new THREE.Color(parameters.insideColor)
-  const colorOutside = new THREE.Color(parameters.outsideColor)
-
-  for (let i = 0; i < parameters.count; i++) {
-    const i3 = i * 3
-
-    // Position
-    const radius = Math.random() * parameters.radius
-    const spinAngle = radius * parameters.spin
-    const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2
-
-    const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
-    const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
-    const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
-
-    positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX // x
-    positions[i3 + 1] = 0 + randomY // y
-    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ // z
-
-    // Color
-    const mixedColor = colorInside.clone()
-    mixedColor.lerp(colorOutside, radius / parameters.radius)
-
-    colors[i3] = mixedColor.r
-    colors[i3 + 1] = mixedColor.g
-    colors[i3 + 2] = mixedColor.b
-  }
-
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-  // Material
-  material = new THREE.PointsMaterial({
-    size: parameters.size,
-    sizeAttenuation: true,
-    depthWrite: false,
-    blending: THREE.AdditiveBlending,
-    vertexColors: true,
-  })
-
-  // Points
-  points = new THREE.Points(geometry, material)
-  scene.add(points)
-}
-
-generateGalaxy()
-
-gui.add(parameters, 'count').min(100).max(10000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'size').min(0.001).max(0.01).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(10).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+scene.add(object1, object2, object3)
 
 /**
  * Sizes
@@ -130,8 +55,6 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
 
